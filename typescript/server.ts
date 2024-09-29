eb180ae3cb18c617852061cbb7a523ab34d06285
Safewaan <safwaanchowdhury26@gmail.com>
2024-09-29 13:16:22 -0400

    init express app

---
+++ typescript/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`App started at http://localhost:${port}`);
+  });
