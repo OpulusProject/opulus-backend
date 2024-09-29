20a1f3f330df38c925ac2d30b6112127e44c40ac
Safewaan <safwaanchowdhury26@gmail.com>
2024-09-29 18:05:08 -0400

    import magic

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,16 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
+import userRouter from "@/routes/userRoutes";
+
 const app = express();
 app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cookieParser());
+
+app.use("/auth", userRouter);
 
 const port = process.env.PORT || 8080;
 
--- typescript/routes/userRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Express, Router } from "express";
-import validateResource from "../middleware/validateResource";
-import { createUserSchema } from "../schema/userSchema";
-
-const userRouter: Router = Router();
-
-userRouter.post("/api/users");
