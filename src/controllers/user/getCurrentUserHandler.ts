e17ba703dda25509d064193db6213104dfba6d48
Safewaan <safwaanchowdhury26@gmail.com>
2025-01-25 01:55:49 -0500

    make opulus great again

--- src/controllers/user/getCurrentUserHandler.ts
+++ src/controllers/user/getCurrentUserHandler.ts
@@ -1,9 +1,8 @@
 import { User } from "@prisma/client";
 import { Request, Response } from "express";
-import { omit } from "lodash";
 
 export function getCurrentUserHandler(req: Request, res: Response) {
   const user = res.locals.user as User;
-  const payload = omit(user, user.password);
-  res.send(payload);
+  // todo: test if this doesnt send password
+  res.send(user);
 }
--- src/services/account/findAccountsByUserId.ts
+++ src/services/account/findAccountsByUserId.ts
@@ -1,9 +1,12 @@
 import prisma from "@prisma/index";
 
+// todo: this needs to receive its filters from query params
 export async function findAccountsByUserId(userId: string) {
   return await prisma.account.findMany({
     where: {
-      userId,
+      item: {
+        userId,
+      },
     },
   });
 }
