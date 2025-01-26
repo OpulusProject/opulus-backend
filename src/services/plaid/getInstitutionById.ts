d946c0f577a2b3d05bfeeadf2f144e77cdb12d7a
Safewaan <safwaanchowdhury26@gmail.com>
2025-01-26 14:00:44 -0500

    x

--- src/services/plaid/getInstitutionById.ts
+++ src/services/plaid/getInstitutionById.ts
@@ -2,7 +2,6 @@ import { CountryCode, InstitutionsGetByIdRequest } from "plaid";
 
 import { plaid } from "./plaid";
 
-// todo: this needs to receive its filters from query params
 export async function getInstitutionById(institutionId: string) {
   const request: InstitutionsGetByIdRequest = {
     institution_id: institutionId,
