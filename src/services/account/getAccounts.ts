40451455f55b1cba15dec0f86be42c04a6167e34
Safewaan <safwaanchowdhury26@gmail.com>
2025-01-26 14:20:17 -0500

    refactor getAccounts

--- src/services/account/getAccounts.ts
+++ src/services/account/getAccounts.ts
@@ -1,7 +1,6 @@
 import prisma from "@prisma/index";
 
-// todo: this needs to receive its filters from query params
-export async function findAccountsByUserId(userId: string) {
+export async function getAccounts(userId: string) {
   return await prisma.account.findMany({
     where: {
       item: {
