c2c690f047fecdec7cb59ea1790c08a414a2bcdd
Safewaan <safwaanchowdhury26@gmail.com>
2025-02-10 00:26:57 -0500

    x

---
+++ src/services/item/updateItem.ts
@@ -0,0 +1,21 @@
+import prisma from "@prisma/index";
+
+interface UpdateItem {
+  plaidId: string;
+  userId: string;
+  accessToken: string;
+  institutionId?: string | null;
+  institutionName?: string | null;
+  requiresUpdate?: boolean;
+}
+
+export async function updateItem(item: UpdateItem) {
+  return await prisma.item.update({
+    where: {
+      plaidId: item.plaidId,
+    },
+    data: {
+      ...item,
+    },
+  });
+}
